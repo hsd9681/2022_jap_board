1e289bb6744639ea7d4ae791a8645b0ba6f3a4eb
hsd9681 <hsd9681@naver.com>
2022-03-28 21:44:22 +0900

    게시물 1개 구현

--- src/main/java/com/hsd/exam/jpaBoard/article/controller/ArticleController.java
+++ src/main/java/com/hsd/exam/jpaBoard/article/controller/ArticleController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usr/article")
@@ -19,5 +20,12 @@ public class ArticleController {
     @ResponseBody
     public List<Article> showList() {
         return articleRepository.findAll();
+    }
+
+    @RequestMapping("detail")
+    @ResponseBody
+    public Article showDetail(long id) {
+        Optional<Article> article = articleRepository.findById(id);//게시물 n번쨰 조회기능,id 매개변수에 nL이 들어옵니다.
+        return article.get();
     }
 }
