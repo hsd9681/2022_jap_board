eaa70b02977b0127788088ee9485473ce239292b
hsd9681 <hsd9681@naver.com>
2022-04-03 15:37:08 +0900

    로그인 기능 추가

--- src/main/java/com/hsd/exam/jpaBoard/User/dao/UserRepository.java
+++ src/main/java/com/hsd/exam/jpaBoard/User/dao/UserRepository.java
@@ -3,6 +3,10 @@ package com.hsd.exam.jpaBoard.User.dao;
 import com.hsd.exam.jpaBoard.User.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
+    boolean existsByEmail(String email);//이렇게 만들어서 사용가능
+
+    Optional<User> findByEmail(String email);
 }
